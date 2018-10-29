0993219a63090b1621f0fabc2d2676ea58604fc5
Jerring <3182730575@qq.com>
2018-10-29 15:34:29 +0800

    Initial commit

---
+++ maoyan.py
@@ -0,0 +1,55 @@
+import json
+import re
+import time
+
+import requests
+from requests import RequestException
+
+
+def get_one_page(url):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/69.0.3497.100 Safari/537.36 '
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+    except RequestException:
+        return None
+
+
+def parse_one_page(html):
+    pattern = re.compile(
+        '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?'
+        'releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
+        re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2].strip(),
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5].strip() + item[6].strip()
+        }
+
+
+def write_to_json(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_json(item)
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(i * 10)
+        time.sleep(1)
